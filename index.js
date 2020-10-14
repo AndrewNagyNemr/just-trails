1f6fcca7f835b3809dfee9900d21b8ce71960161
Andrew N <Andrew.N@trufla.com>
2020-10-14 09:28:16 +0200

    -adding event to the btn instead of using onclick adding an ajax request to get the token before getting the actual data from the server

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const form = document.querySelector(".form");
+const btn = document.querySelector("#btn");
 
 let name, email, address, phone;
 
-function submitForm() {
+function submitForm(api_token) {
   name = form.name.value;
   email = form.email.value;
   address = form.address.value;
   phone = form.phone.value;
 
-  sendRequest({ name, email, address, phone }, 123);
+  sendRequest({ name, email, address, phone }, api_token);
 }
 
 function sendRequest(args, api_token) {
-  fetch("http://localhost:4200/api/leads", {
+  fetch("http://localhost:4200/api/token", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ data: { ...args }, api_token: api_token }),
-  }).then(function (response) {
-    // return response.json();
+    //provide the credentials to get the token
+    body: JSON.stringify(),
+  }).then((response) => {
+    fetch("http://localhost:4200/api/leads", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data: { ...args },
+        api_token: response.api_token,
+      }),
+    }).then((response) => {
+      // return response.json();
+    });
   });
 }
 
-form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
+
+btn.addEventListener("click", (e) => {
   submitForm();
 });
